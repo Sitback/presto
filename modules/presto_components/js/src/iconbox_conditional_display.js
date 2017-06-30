afc7add36e5a9e3eb9c755070254363b83a596c3
Chin Godawita <chin@sitback.com.au>
2017-06-30 12:15:25 +1000

    Fixed all icon boxes being triggered on type change.

--- modules/presto_components/js/src/iconbox_conditional_display.js
+++ modules/presto_components/js/src/iconbox_conditional_display.js
@@ -9,44 +9,50 @@
    * Attaches the behavior of the media entity browser view.
    */
   Drupal.behaviors.paragraphIconBox = {
-    attach(context, settings) {
-      let conditionalDisplay = (elt) => {
-        let id = $(elt).attr('id');
-        let selected = $(`#${id}`).find('option:selected').val();
-
-        let imageField = $('[data-drupal-selector*="edit-field-body-paragraphs-"][data-drupal-selector*="-subform-field-media-wrapper"]');
-        let iconField = $('[data-drupal-selector*="edit-field-body-paragraphs-"][data-drupal-selector*="-subform-field-icon-wrapper"]');
+    attach(context/* , settings */) {
+      let conditionalDisplay = (event) => {
+        let $target = $(event.target);
+        let selected = $target.find('option:selected').val();
+        let $parentForm = $target.parents('.paragraphs-subform').first();
+        let $imageField = $parentForm.find(
+          '[data-drupal-selector*="-subform-field-media-wrapper"]'
+        );
+        let $iconField = $parentForm.find(
+          '[data-drupal-selector*="-subform-field-icon-wrapper"]'
+        );
 
         switch (selected.toLowerCase()) {
           case '_none':
-            imageField.hide();
-            iconField.hide();
+            $imageField.hide();
+            $iconField.hide();
             break;
 
           case 'image':
-            imageField.show();
-            iconField.hide();
+            $imageField.show();
+            $iconField.hide();
             break;
 
           case 'icon':
-            iconField.show();
-            imageField.hide();
+            $iconField.show();
+            $imageField.hide();
             break;
 
           default:
-            // Case default or _none.
-            // Hide both.
-            imageField.hide();
-            iconField.hide();
+            // Case default or _none: hide both.
+            $imageField.hide();
+            $iconField.hide();
             break;
         }
       };
 
-      $('[name*="field_body_paragraphs"][name*="subform"][name*="field_icon_box_type"]').each(function () {
-        $(this).load(conditionalDisplay(this));
-        $(this).change(function () {
-          conditionalDisplay(this);
-        });
+      // Trigger on each 'icon box' component form.
+      $(
+        '[name*="field_body_paragraphs"][name*="subform"][name*="field_icon_box_type"]',
+        context
+      ).once('paragraphIconBox').each(function () {
+        let $this = $(this);
+        $this.on('change', conditionalDisplay);
+        $this.trigger('change');
       });
     }
   };
